9782cc4e321c511882568b5ce97970a5ca72192b
hanmou1021 <102566152+hanmou1021@users.noreply.github.com>
2023-08-15 18:04:25 -0400

    Update main.py

--- Slack-notice/main.py
+++ Slack-notice/main.py
@@ -6,38 +6,62 @@ import json
 import slack
 from datetime import datetime
 
-now = datetime.now()
-dscp=yfinance.download("QQQ",period="1hour",interval = "1m")
-current_price=str(dscp['Close'][-1])
+### Notice function
 
-ds = yfinance.download("QQQ",period="7d",interval = "1h")
+def SlackNotice(name):
+    stock=name
 
-slack_token='xoxp-5130203815986-5142830602273-5115876156183-55f103180df997bac2563377609fa0ff'
-client=slack.WebClient(token=slack_token)
+    ### Set up Slack Workspace Bot client
 
-def stra(request):
-
+    slack_token='slack-secret'
+    client=slack.WebClient(token=slack_token)
+    now = datetime.now()
+    dscp=yfinance.download(stock,period="1hour",interval = "1m")
+    current_price=str(dscp['Close'][-1])
+    ds = yfinance.download(stock,period="7d",interval = "1h")
+    
+    ### Build signals
+    
     ema5 = ds["Close"].ewm(span=5, adjust=False, min_periods=5).mean()
     ema15 = ds["Close"].ewm(span=15, adjust=False, min_periods=15).mean()
-
-
     k = ds["Close"].ewm(span=12, adjust=False, min_periods=12).mean()
     d = ds["Close"].ewm(span=26, adjust=False, min_periods=26).mean()
-
     macd = k - d
     macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
     macd_h = macd - macd_s
+
+    ### Build Conditions 
+    
     if ema5[-3]-ema15[-3]<0 and ema5[-2]-ema15[-2]>0:
         #####################################create_order("SPY", 2490, "buy", "market", "gtc")
-        
-        notice=str(now)+'  '+'QQQ BUY '+ current_price
-        client.chat_postMessage(channel='#qqq-hour-line',text=notice)
-        
+        ChannelText='#'+stock+'-hour-line'
+        notice=str(now)+'  '+stock+' BUY '+ current_price
+        client.chat_postMessage(channel=ChannelText,text=notice)
         
     elif (ema5[-3]-ema15[-3]>0 and ema5[-2]-ema15[-2]<0) or (ema5[-2]-ema15[-2]>0 and macd_h[-2]<0 and macd_h[-3]>0):
         ########################################create_order("SPY", 2490, "sell", "market", "gtc")
-        
-        notice=str(now)+'  '+'QQQ SELL '+ current_price
-        client.chat_postMessage(channel='#qqq-hour-line',text=notice)
+        ChannelText='#'+stock+'-hour-line'
+        notice=str(now)+'  '+stock+' SELL '+ current_price
+        client.chat_postMessage(channel=ChannelText,text=notice)
         
     return {"status code":200}
+
+### Triggered function
+
+def SendNotice(request):
+    list=['SPY','QQQ','DIA']
+    
+    ### Add error handling
+    
+    try:
+        for i in list:
+            SlackNotice(i)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+    else:
+        message='status code:200'
+
+    return message
+   
+    
